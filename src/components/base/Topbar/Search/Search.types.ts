f049c021ca6c3f9e610fed100ef811f5418ab328
ShakedZrihen <shaked.zrihen@gmail.com>
2024-09-24 16:29:46 +0300

    feat: add searchFocus and blur as props

--- src/components/base/Topbar/Search/Search.types.ts
+++ src/components/base/Topbar/Search/Search.types.ts
@@ -1,5 +1,7 @@
 export interface SearchProps {
   onSearch?: (searchTerm: string, searchType: string) => void;
+  onSeachFocus?: () => void;
+  onSearchBlur?: () => void;
   withSearch?: boolean;
   searchSettingsStyleOverrides?: string;
 }
