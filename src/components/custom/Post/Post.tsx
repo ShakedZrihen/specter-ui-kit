290503d03f26578952d83be8ec4dbca657235306
y1234n5 <yn1.netivot@proton.me>
2024-09-25 16:15:56 +0300

    fix css of sourcename, check if channel name exists and handle if so

--- src/components/custom/Post/Post.tsx
+++ src/components/custom/Post/Post.tsx
@@ -81,7 +81,8 @@ export function Post(props: PostProps & { className?: string }) {
             <Link href={source.url} target='_blank'>
               {cleanProtocol(source.url)}
             </Link>
-            •
+            {source.channelName ? (<>
+              •
             <Link href={source.channelUrl} target='_blank'>
               <ChannelName
                 direction={getTextDirection(franc(source.channelName))}
@@ -89,7 +90,9 @@ export function Post(props: PostProps & { className?: string }) {
                 {source.channelName}
               </ChannelName>
             </Link>{' '}
-            • <Typography>{source.sourceName}</Typography>
+            </>)
+            : (null)}
+            • <Typography style={{ paddingRight: "0.2rem" }}>{source.sourceName}</Typography>
           </PostSource>
         </PostHeaderContent>
         {!slimView && (
