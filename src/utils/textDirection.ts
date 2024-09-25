5147dcdda2b09005a10f32b75a3e409597505114
ShakedZrihen <shaked.zrihen@gmail.com>
2024-09-25 14:12:38 +0300

    fix: franc dir was parsed incorrect

--- src/utils/textDirection.ts
+++ src/utils/textDirection.ts
@@ -1,4 +1,4 @@
-const RTL_LANGUAGES = ['ara', 'heb'];
+const RTL_LANGUAGES = ['arb', 'heb'];
 
 export function getTextDirection(language: string): 'rtl' | 'ltr' {
   return RTL_LANGUAGES.includes(language) ? 'rtl' : 'ltr';
