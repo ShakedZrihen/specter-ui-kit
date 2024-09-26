7a161f407258c184de1a8650a7978d9e03115c2a
pri-nb <157955540+pri-nb@users.noreply.github.com>
2024-09-26 11:29:31 +0300

    Chore(SystemChip): reformat

--- src/components/base/SystemChip/SystemChip.style.tsx
+++ src/components/base/SystemChip/SystemChip.style.tsx
@@ -3,24 +3,24 @@ import { styled } from '@mui/material';
 export const StyledSystemChip = styled('div')``;
 
 export const StatusChip = styled('button')`
-	border-radius: 9999px;
-    font-weight: 400;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-style: none; 
-    gap: 0.5rem;
-    width: 4rem;
-    height: 2rem;
+  border-radius: 9999px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-style: none;
+  gap: 0.5rem;
+  width: 4rem;
+  height: 2rem;
 `;
 
 export const InfoChip = styled('button')`
-    align-items: center;
-    justify-content: center;
-    border-style: none; 
-    gap: 0.5rem;
-    width: 5rem;
-    height: 2rem;
-    	flex-direction: row;
-    border-radius: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  border-style: none;
+  gap: 0.5rem;
+  width: 5rem;
+  height: 2rem;
+  flex-direction: row;
+  border-radius: 0.5rem;
 `;
--- src/components/base/SystemChip/SystemChip.tsx
+++ src/components/base/SystemChip/SystemChip.tsx
@@ -1,25 +1,31 @@
 import { StatusChip, InfoChip } from './SystemChip.style';
 
 export interface SystemChipProps {
-    bgColor: "#BFECB0" | "#FFE7C2" | "#FFC2C2" | "#F2F3F6";
-    icon?: any;
-    onClick: () => null;
-    label: string;
+  bgColor: '#BFECB0' | '#FFE7C2' | '#FFC2C2' | '#F2F3F6';
+  icon?: any;
+  onClick: () => null;
+  label: string;
 }
 
-
 export function SystemChip({ bgColor, label, onClick, icon }: SystemChipProps) {
-  if(icon) {
-    return <InfoChip style={{
-      display: "flex",
-      flexDirection: "row", 
-      alignItems: "center", 
-      justifyContent: "center", 
-    }} onClick={onClick}>
-      {icon} {label}
-    </InfoChip>
+  if (icon) {
+    return (
+      <InfoChip
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onClick={onClick}
+      >
+        {icon} {label}
+      </InfoChip>
+    );
   }
-  return <StatusChip onClick={onClick} style={{ background: bgColor }}>
-    {label}
-    </StatusChip>;
+  return (
+    <StatusChip onClick={onClick} style={{ background: bgColor }}>
+      {label}
+    </StatusChip>
+  );
 }
