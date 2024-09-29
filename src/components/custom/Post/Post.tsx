2f59aa7341cf0f980bae99ae4c0d264df01e45f2
ShakedZrihen <shaked.zrihen@gmail.com>
2024-09-29 14:49:18 +0300

    fix: fix label

--- src/components/custom/Post/Post.tsx
+++ src/components/custom/Post/Post.tsx
@@ -106,7 +106,7 @@ export function Post(props: PostProps & { className?: string }) {
                   }
                 />
               }
-              label='Mark as read'
+              label='סימון כנקרא'
             />
           </PostReadIndicator>
         )}
