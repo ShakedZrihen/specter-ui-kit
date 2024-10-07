c0303de34c286009d2cdd3e7bc4caa97a2005fa0
ShakedZrihen <shaked.zrihen@gmail.com>
2024-10-07 12:02:35 +0300

    fix: use fallback hebrew

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'he',
     debug: true,
     resources: {
       en: { translation: en },
