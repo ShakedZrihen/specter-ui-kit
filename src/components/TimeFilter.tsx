102485dacdaa5b380f27798899214eb2fb309086
shaked zohar <shaked.zrihen@gmail.com>
2025-07-09 22:00:54 +0300

    Follow-up implementation for 'All requirements implemented with meaningful filenames' in issue #93

--- src/components/TimeFilter.tsx
+++ src/components/TimeFilter.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { LocalizationProvider, DesktopDateTimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { StyledDesktopDateTimePicker } from './StyledDesktopDateTimePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateAdapter } from '@mui/x-date-pickers/models';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 interface TimeFilterProps {
   value: string | Date | null;
   defaultValue?: string | Date | null;
   label?: string;
   onChange: (date: string | null) => void;
-  dateParser?: (date: string | Date | null) => dayjs.Dayjs | null;
+  dateParser?: (date: string | Date | null) => Dayjs | null;
   printAs?: string;
-  dateAdapter?: DateAdapter;
+  dateAdapter?: any;
 }
 
 const TimeFilter: React.FC<TimeFilterProps> = ({
@@ -28,7 +28,7 @@ const TimeFilter: React.FC<TimeFilterProps> = ({
 
   return (
     <LocalizationProvider dateAdapter={dateAdapter}>
-      <DesktopDateTimePicker
+      <StyledDesktopDateTimePicker
         label={label}
         value={parsedValue}
         onAccept={(date) => {
