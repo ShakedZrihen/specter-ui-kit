317b803689f82ec96d4f71ca2706b83eae0cb3e3
shaked zohar <shaked.zrihen@gmail.com>
2025-07-09 22:03:30 +0300

    Follow-up implementation for 'Unit tests for all new functions/classes' in issue #93

--- src/components/StyledDesktopDateTimePicker.tsx
+++ src/components/StyledDesktopDateTimePicker.tsx
@@ -1,6 +1,29 @@
+import React from 'react';
 import { DesktopDateTimePicker } from '@mui/x-date-pickers';
-import { styled } from '@mui/material/styles';
 
-export const StyledDesktopDateTimePicker = styled(DesktopDateTimePicker)({
-  // Add custom styles here if needed
-});
+interface StyledDesktopDateTimePickerProps {
+  label?: string;
+  value: any;
+  onAccept: (date: any) => void;
+  format: string;
+  defaultValue: any;
+}
+
+export const StyledDesktopDateTimePicker: React.FC<StyledDesktopDateTimePickerProps> = ({
+  label,
+  value,
+  onAccept,
+  format,
+  defaultValue,
+}) => {
+  return (
+    <DesktopDateTimePicker
+      label={label}
+      value={value}
+      onChange={onAccept}
+      inputFormat={format}
+      defaultValue={defaultValue}
+      renderInput={(params) => <input {...params} />}
+    />
+  );
+};
