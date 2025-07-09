d8405a359c0ea356639161a7cad27c1eea756b28
shaked zohar <shaked.zrihen@gmail.com>
2025-07-09 22:02:04 +0300

    Follow-up implementation for 'Code follows .cursor-rules.yaml conventions (PEP8, type hints, Python 3.12)' in issue #93

---
+++ src/components/StyledDesktopDateTimePicker.tsx
@@ -0,0 +1,6 @@
+import { DesktopDateTimePicker } from '@mui/x-date-pickers';
+import { styled } from '@mui/material/styles';
+
+export const StyledDesktopDateTimePicker = styled(DesktopDateTimePicker)({
+  // Add custom styles here
+});
